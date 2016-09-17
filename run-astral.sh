eff0855c46ddb0eb0c1e82f8555a819d43e2be81
Erfan Sayyari <esayyari@eng.ucsd.edu>
2016-09-17 14:52:45 -0700

    change in run-astral.sh

--- run-astral.sh
+++ run-astral.sh
@@ -13,12 +13,12 @@ tmpdir=`mktemp -d`
 cd $tmpdir
 
 head -n $G $H/$ID/$ALIGNNAME > estimated"$G".gene_trees.trees
-
-java -Xmx4000M -jar $WS_HOME/ASTRAL/$ast -i estimated"$G".gene_trees.trees -o estimated"$G".species_tree.trees > info."$G".log 2>&1
+mkdir -p $H/$ID/ASTRAL_ML_SPECIES"$G"
+java -jar $WS_HOME/ASTRAL/$ast -i estimated"$G".gene_trees.trees -o estimated"$G".species_tree.trees > $H/$ID/ASTRAL_ML_SPECIES"$G"/info."$G".log 2>&1
 test $? -ne "0" && echo "something was wrong" && exit 1
 k=$(basename $tmpdir)
 j=$(dirname $tmpdir)
 cd ../
-mv $k $H/$ID/ASTRAL_ML_SPECIES"$G"
+mv $k/* $H/$ID/ASTRAL_ML_SPECIES"$G"/
 echo "Done"
 
