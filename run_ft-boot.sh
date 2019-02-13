1551bcaf3a8fa993a48be624d35fe9c07ce92d05
esayyari <esayyari@eng.ucsd.edu>
2019-02-13 13:20:48 -0800

    Update run_ft-boot.sh

--- run_ft-boot.sh
+++ run_ft-boot.sh
@@ -6,12 +6,8 @@ ending=$3
 
 for i in `seq $starting $ending`; do
 	command=$(sed -n "$i,$i p" $file);
-<<<<<<< HEAD
-	eval "${command}"
-=======
 	eval ${command};	
 	test #? -ne 0 && echo "something happend!" && exit 1
->>>>>>> a8e94dc7d9079c64029e7ee2512fab9b5f87bfea
 done
 echo "done"
 	
